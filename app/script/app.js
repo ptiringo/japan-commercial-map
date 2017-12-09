bd4e1551db67a34e5a11e1f787bba4983fb6f4bb
ptiringo <ptiringo@gmail.com>
2017-12-10 00:03:22 +0900

    Implement basic html

--- app/script/app.js
+++ app/script/app.js
@@ -1,7 +1,11 @@
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
+let map;
+
+initMap = () => {
+    fetch('../api/downtowns')
+        .then(response => {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 35.685175, lng: 139.7528 },
+                zoom: 10
+            });
+        })
 }
