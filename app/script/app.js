1c9dbe93e24ea5751cf6e0656f28eeed55b4fedf
ptiringo <ptiringo@gmail.com>
2017-12-21 20:17:17 +0900

    Add loadGeoJson call

--- app/script/app.js
+++ app/script/app.js
@@ -1,11 +1,13 @@
 let map;
 
 initMap = () => {
-    fetch('../api/downtowns')
-        .then(response => {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: { lat: 35.685175, lng: 139.7528 },
-                zoom: 10
-            });
-        })
-}
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 35.685175,
+            lng: 139.7528
+        },
+        zoom: 10
+    });
+
+    map.data.loadGeoJson('../api/downtowns/')
+};
