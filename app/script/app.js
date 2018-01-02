0b44d83cf7995bc077af9b4f37c9e419c4733da9
ptiringo <ptiringo@gmail.com>
2018-01-02 21:19:43 +0900

    Add ESLint settigs and more

--- app/script/app.js
+++ app/script/app.js
@@ -1,37 +1,33 @@
-let map;
+window.initMap = () => {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 35.685175,
+      lng: 139.7528,
+    },
+    zoom: 10,
+  });
 
-initMap = () => {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 35.685175,
-            lng: 139.7528
-        },
-        zoom: 10
-    });
+  map.data.loadGeoJson('../api/downtowns/');
 
-    map.data.loadGeoJson('../api/downtowns/');
+  const colors = new Map([
+    [11, 'red'],
+    [12, 'blue'],
+    [13, 'green'],
+    [14, 'black'],
+    [15, 'yellow'],
+  ]);
 
-    const colors = new Map([
-        [11, 'red'],
-        [12, 'blue'],
-        [13, 'green'],
-        [14, 'black'],
-        [15, 'yellow'],
-    ])
-
-
-    map.data.setStyle(feature => {
-        return {
-            title: feature.getProperty('name'),
-            icon: {
-                path: google.maps.SymbolPath.CIRCLE,
-                fillColor: colors.get(feature.getProperty('commercialAccumulationCode')),
-                fillOpacity: .5,
-                scale: Number(feature.getProperty('annualSalesTurnover')) / 10000,
-                strokeColor: 'white',
-                strokeWeight: .5
-            }
-        };
-    });
-
+  map.data.setStyle(feature => (
+    {
+      title: feature.getProperty('name'),
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: colors.get(feature.getProperty('commercialAccumulationCode')),
+        fillOpacity: 0.5,
+        scale: Number(feature.getProperty('annualSalesTurnover')) / 10000,
+        strokeColor: 'white',
+        strokeWeight: 0.5,
+      },
+    }
+  ));
 };
